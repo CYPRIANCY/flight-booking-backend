bfc54253d2ac9d757356df9320480843c8ca0698
AKPEN SESUGH CYPRIAN <120281513+CYPRIANCY@users.noreply.github.com>
2026-08-24 15:29:04 +0100

    Add error handler middleware to server

--- server.js
+++ server.js
@@ -31,6 +31,8 @@ app.use('/api/payments', paymentRoutes);
 app.use('/api/admin', adminRoutes);
 app.use('/api/tickets', ticketRoutes);
 
+app.use(errorHandler);
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port http://localhost:${PORT}`));
