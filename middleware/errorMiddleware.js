854fe5dfffb08c9788724084a0f1c4f66fc97a44
AKPEN SESUGH CYPRIAN <120281513+CYPRIANCY@users.noreply.github.com>
2026-08-25 13:59:06 +0100

    Update errorMiddleware.js

--- middleware/errorMiddleware.js
+++ middleware/errorMiddleware.js
@@ -1,15 +1,23 @@
-const errorHandler = (err, req, res, next) => {
+const errorHandler = (
+  err,
+  req,
+  res,
+  next
+) => {
   console.error(err.stack);
 
-  const statusCode = res.statusCode === 200
-    ? 500
-    : res.statusCode;
+  const statusCode =
+    res.statusCode === 200
+      ? 500
+      : res.statusCode;
 
   res.status(statusCode).json({
-    message: err.message,
-    stack: process.env.NODE_ENV === 'production'
-      ? undefined
-      : err.stack
+    message:
+      err.message || 'Internal Server Error',
+
+    ...(process.env.NODE_ENV === 'development'
+      ? { stack: err.stack }
+      : {})
   });
 };
 
